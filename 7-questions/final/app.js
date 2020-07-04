c53970de7ba8c6830e25e355fed04ec03aa90d8e
jaisondza <jaisonj1010@gmail.com>
2020-07-04 16:59:48 +0530

    questions project updated

--- 7-questions/final/app.js
+++ 7-questions/final/app.js
@@ -8,13 +8,13 @@ questions.forEach(function (question) {
   btn.addEventListener("click", function () {
     // console.log(question);
 
-    questions.forEach(function (item) {
-      if (item !== question) {
+    questions.forEach(function (item) {              // this forEach is for closing other question's details. 
+      if (item !== question) {                       // checking if the question am inside which, is not the question selected in this forEach loop. This is used to open only one question and close others.
         item.classList.remove("show-text");
       }
     });
 
-    question.classList.toggle("show-text");
+    question.classList.toggle("show-text");     // showing details of the one selected. 
   });
 });
 
@@ -23,7 +23,7 @@ questions.forEach(function (question) {
 
 // btns.forEach(function (btn) {
 //   btn.addEventListener("click", function (e) {
-//     const question = e.currentTarget.parentElement.parentElement;
+//     const question = e.currentTarget.parentElement.parentElement;      // going to parent elements.
 
 //     question.classList.toggle("show-text");
 //   });
