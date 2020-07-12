91aa385d7cd163954014a623531514ab25920184
jaisondza <jaisonj1010@gmail.com>
2020-07-13 04:06:44 +0530

    question card front end

--- 17-new-flashcards/flashcards-setup/js/app.js
+++ 17-new-flashcards/flashcards-setup/js/app.js
@@ -18,12 +18,39 @@ function eventListeners(){
     // show question form
     showBtn.addEventListener("click", function(){
         ui.showQuestion(questionCard);
-    })
+    });
+    // hide form
+    closeBtn.addEventListener('click', function(){
+        ui.hideQuestion(questionCard);
+    });
+    // add quesion- form submission
+    form.addEventListener('submit', function(e){
+        e.preventDefault();
+        
+        const questoinValue = questionInput.value;
+        const answerValue = answerInput.value;
+
+        if(questoinValue === '' ||answerValue === ''){
+            feedback.classList.add('showItem', 'alert-danger');  //feedback is shows only if its empty.
+            feedback.textContent = 'Cannot add empty values!';
+
+            setTimeout(function(){
+                feedback.classList.remove('showItem', 'alert-danger');
+            }, 2000)
+        }
+        else{
+            const question = new Question(id, title, answer);
+            data.push(question);
+            id++;
+            ui.clearFields();
+        }
+    });
 }
 
 // UI constructor
 function UI(){}
 
+// show question from
 UI.prototype.showQuestion = function(questionCard){
     questionCard.classList.add('showItem');          // this shows the question-anwer form.
 }
@@ -36,8 +63,10 @@ UI.prototype.hideQuestion = function(questionCard){
 
 
 // question constructor
-function Question(){
-
+function Question(id, title, answer){
+    this.id = id;
+    this.answer = answer;
+    this.title = title;
 }
 
 
