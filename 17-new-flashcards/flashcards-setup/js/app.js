22d117e2ec3db9c13a017a1ce3da9f365d58331f
jaisondza <jaisonj1010@gmail.com>
2020-07-13 17:15:26 +0530

    question project done

--- 17-new-flashcards/flashcards-setup/js/app.js
+++ 17-new-flashcards/flashcards-setup/js/app.js
@@ -43,9 +43,55 @@ function eventListeners(){
             data.push(question);
             id++;
             ui.addQuestion(questionList, question);
-            ui.clearFields(questoinValue, answerInput);
+            ui.clearFields(questionInput, answerInput);
+            ui.hideQuestion(questionCard);
         }
     });
+
+    // working with a question. The question which is created and deployed.
+    questionList.addEventListener('click', function(e){ // listening to all the buttons inside the box.
+        e.preventDefault();
+        console.log(e.target); // this gives the class div of the button being pressed.
+        if(e.target.classList.contains('delete-flashcard')){  // since we are listening to entire question box, checking where exactly the user is clicking.
+            questionList.removeChild(e.target.parentElement.parentElement.parentElement); // this points to the div created dynamically when question is created with the class "col-md-4".
+            // console.log(e.target.parentElement.parentElement.parentElement); // that div makes one question. So deleting from there entirely.
+            let id = e.target.dataset.id;
+            // rest of the items
+            const tempData = data.filter(function(item){
+                return item.id !== parseInt(id);
+            });
+            console.log("Before deleting ", data);
+            // editing the array
+            data = tempData;
+            console.log("After deleting ", data)
+        }
+
+        else if(e.target.classList.contains('edit-flashcard')){
+            // get id
+            let id = e.target.dataset.id;
+            // delete from the DOM.
+            questionList.removeChild(e.target.parentElement.parentElement.parentElement);
+            ui.showQuestion(questionCard);
+            // add the values to the form and display.
+            const tempQuestion = data.filter(function(item){
+                return item.id === parseInt(id);
+            });
+            
+            // rest of the data.
+            const tempData = data.filter(function(item){
+                return item.id !== parseInt(id);
+            });
+            console.log("Before editing ", data);
+            data = tempData;  // the array has to be updated without the value which is being edited.
+            console.log("After editing ", data);
+            // inserting the values to be edited into the form
+            questionInput.value = tempQuestion[0].title;
+            answerInput.value = tempQuestion[0].answer;
+        }   
+        else if (event.target.classList.contains("show-answer")){
+            e.target.nextElementSibling.classList.toggle('showItem'); // nextElementSibling points to the element in the same line which is h5 here. That has class showItem hidden which has CSS property. Adding that class shows the item and removing hides.
+        }
+    })
 }
 
 // UI constructor
@@ -75,13 +121,14 @@ UI.prototype.addQuestion = function(element, question){  // element is where we 
      
         <a href="#" id="edit-flashcard" class=" btn my-1 edit-flashcard text-uppercase" data-id="
         ${question.id}">edit</a>
-        <a href="#" id="delete-flashcard" class=" btn my-1 delete-flashcard text-uppercase">delete</a>
+        <a href="#" id="delete-flashcard" class=" btn my-1 delete-flashcard text-uppercase" data-id="
+        ${question.id}">delete</a>
         </div>
     </div>
     `;
-
+    // above data-id is added dynamically to the edit button which we use while editing the question.
     element.appendChild(div);
-}
+};
 // clear fields
 UI.prototype.clearFields = function(question, answer){
     question.value = '';
