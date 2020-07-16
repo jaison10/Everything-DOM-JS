6fbd0e206bb15a43bd41c10d95c4df15a95898eb
jaisondza <jaisonj1010@gmail.com>
2020-07-16 11:29:04 +0530

    using fetch()

--- 18-AJAX-API/1-chuck-norris-jokes/setup/app.js
+++ 18-AJAX-API/1-chuck-norris-jokes/setup/app.js
@@ -4,37 +4,22 @@ const img = document.querySelector('.container img');
 
 const url = "https://api.chucknorris.io/jokes/random";
 
-btn.addEventListener('click', async () => {
-    getData(url);
+btn.addEventListener('click',()=> {
+    fetch(url)
+    .then((data) => data.json())
+    .then(response => displayData(response)) // the second 'then' because .json() returns promise.
+    .catch(err=> console.log(err));  
 });
 
-function getData(url){
-    const xhr =  new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.send();
-    console.log(xhr);
-    xhr.onreadystatechange = ()=>{
-        if(xhr.readyState !== 4){
-            return;
-        }
-        if(xhr.status === 200){
-            img.classList.add("shake-img");
-            const {value : joke } = JSON.parse(xhr.responseText);  // {value } is destructuring the object.
-            // console.log(response.value);
-            content.textContent = joke;
-            const random = Math.random()*1000;
-            console.log(random);
-            
-            setTimeout(()=>{
-                img.classList.remove("shake-img");
-            }, random)
-        }
-        else{
-            console.log(
-             {
-                status: xhr.status,
-                text: xhr.statusText
-            });
-        }
-    }
+function displayData(response){
+    img.classList.add("shake-img");
+    const {value : joke } = response;  // {value } is destructuring the object.
+    // console.log(response.value);
+    content.textContent = joke;
+    const random = Math.random()*1000;
+    console.log(random);
+    
+    setTimeout(()=>{
+        img.classList.remove("shake-img");
+    }, random)
 }
